1979deba5ad59f4b1d11b9312338e4c9627a5f77
rutvik-pimple <48850558+rutvik-pimple@users.noreply.github.com>
2020-10-21 21:26:52 +0530

    done added button and para

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { paragraph } from "./paragraph";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = { str: [] };
+  }
+  addPara = (para) => {
+    const copyStr = this.state.str.append(para);
+    this.setState({ str: copyStr });
+  };
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button
+          id="click"
+          onClick={this.addPara(
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          )}
+        >
+          renderPara
+        </button>
+        {this.state.str.map((string) => (
+          <paragraph id="para" para={string} />
+        ))}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
